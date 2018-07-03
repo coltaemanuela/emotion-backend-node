d639dbe026471ee08bb4f21ec00f35c3e38c1bab
coltaemanuela <coltaemanuela@gmail.com>
2018-07-03 13:16:09 +0100

    modify project structure; improve modularity

--- app.js
+++ app.js
@@ -3,22 +3,26 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var fs = require('fs');
 var https = require('https'); 
-var DeepAffects = require('deep-affects');
 var request = require('request');
-var jsonrequest = require('jsonrequest');
+var rs = require('./service/apiResponse.js');
 var config = require('./config/config.json');
+var general = require('./routes/general');
+var api = require('./routes/api');
 var app = express();
 
 //setup view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use('/api', api)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //homepage
 app.get('/', function (req, res) {
   const API_ENDPOINT = 'https://api.webempath.net/v2/analyzeWav';
   var formData = {
   apikey: config.empath_API_key,
-  wav: fs.createReadStream("./resources/0wuqx-scsny.wav") //should not be .wav format, should not exceed 5seconds, 1.9MB  and frequency should be 11025 Hz
+  wav: fs.createReadStream("./resources/0wuqx-scsny.wav") //should be .wav format, shouldn't exceed 5s, 1.9MB  and frequency should be 11025 Hz
   };
 
   request.post({ url: API_ENDPOINT, formData: formData }, function(err, response) {
@@ -32,25 +36,6 @@ app.get('/', function (req, res) {
   });
 });
 
-
-app.get('/affects', function(req,res){
-
-  var defaultClient = DeepAffects.ApiClient.instance;
-  // Configure API key authorization: UserSecurity
-  var UserSecurity = defaultClient.authentications['UserSecurity'];
-  UserSecurity.apiKey = config.deepaffect_API_key;
-  var apiInstance = new DeepAffects.EmotionApi();
-  var body = DeepAffects.Audio.fromFile("./resources/female600.wav");  //source: http://www.signalogic.com/melp/EngSamples/female600.wav
-
-  jsonrequest("https://proxy.api.deepaffects.com/audio/generic/api/v1/sync/recognise_emotion?apikey=Y72YkFQJ6etxIpLyyzWhUqtoEdLOC1KE", body, (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-    console.log(data);
-    }
-  });
-
-});
 
 //server
 app.listen(config.server.port, function () {
---
+++ routes/api.js
@@ -0,0 +1,27 @@
+var config = require('../config/config.json');
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require("fs");
+var DeepAffects = require('deep-affects');
+var jsonrequest = require('jsonrequest');
+var router = express.Router();
+
+router.get('/affects', function(req,res,next){    
+    var defaultClient = DeepAffects.ApiClient.instance;
+    // Configure API key authorization: UserSecurity
+    var UserSecurity = defaultClient.authentications['UserSecurity'];
+    UserSecurity.apiKey = config.deepaffect_API_key;
+    var apiInstance = new DeepAffects.EmotionApi();
+    var body = DeepAffects.Audio.fromFile("./resources/female600.wav");  //source: http://www.signalogic.com/melp/EngSamples/female600.wav. Length:49s
+
+    jsonrequest("https://proxy.api.deepaffects.com/audio/generic/api/v1/sync/recognise_emotion?apikey=Y72YkFQJ6etxIpLyyzWhUqtoEdLOC1KE", body, (err, data) => {
+    if (err) {
+        console.error(err);
+    } else {
+    console.log(data);
+    }
+    });
+    next();
+});
+
+module.exports = router;
