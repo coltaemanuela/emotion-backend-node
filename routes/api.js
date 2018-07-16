279c352571460477ee4d58172defc8ad4479f4bd
coltaemanuela <coltaemanuela@gmail.com>
2018-07-16 16:46:30 +0100

    convert-to-wav API using ffmpeg

--- routes/api.js
+++ routes/api.js
@@ -10,6 +10,7 @@ var DeepAffects = require('deep-affects');
 var jsonrequest = require('jsonrequest');
 var firebase = require('firebase');
 var multer = require('multer');
+var ffmpeg = require('fluent-ffmpeg');
 var expressValidator = require('express-validator');
 var router = express.Router();
 
@@ -53,21 +54,31 @@ router.post("/upload", multer({ dest: './uploads/'}).array('newfile', 10), funct
   next();
 });
 
-
-
 router.post('/transcript', function(req,res,next){
-
   fs.writeFile("transcripts/"+ req.body.document_id+".txt", req.body.message, function(err) {
     if(err) {
         return console.log(err);
     }  
-    console.log("The file was saved!");
     res.json("The file was saved!");
     next();
   }); 
-
 });
 
+
+router.post('/convert-to-wav', multer({ dest: './uploads/wav_files'}).array('audio_file', 10), function(req,res, next){
+  let track = req.files;   // './source.mp3';
+  ffmpeg(track).toFormat('wav').on('error', (err) => {
+      // console.log('An error occurred: ' + err.message);
+      return res.json(apiResponse.customError(err.message));
+  }).on('progress', (progress) => {
+      console.log('Processing: ' + progress.targetSize + ' KB converted');
+  }).on('end', () => {
+    // res.json();
+      // console.log('Processing finished!');
+      return res.json(apiResponse.success('Processing finished!'));
+  }).save('./hello.wav')
+  next();
+});
 
 
 router.get('/affects', function(req,res,next){    
@@ -76,7 +87,7 @@ router.get('/affects', function(req,res,next){
   var UserSecurity = defaultClient.authentications['UserSecurity'];
   UserSecurity.apiKey = config.deepaffect_API_key;
   var apiInstance = new DeepAffects.EmotionApi();
-  var body = DeepAffects.Audio.fromFile("../resources/female600.wav");  //source: http://www.signalogic.com/melp/EngSamples/female600.wav. Length:49s
+  var body = DeepAffects.Audio.fromFile("./resources/female600.wav");  //source: http://www.signalogic.com/melp/EngSamples/female600.wav. Length:49s
   jsonrequest("https://proxy.api.deepaffects.com/audio/generic/api/v1/sync/recognise_emotion?apikey=Y72YkFQJ6etxIpLyyzWhUqtoEdLOC1KE", body, (err, data) => {
   if (err) {
       console.error(err);
@@ -87,33 +98,31 @@ router.get('/affects', function(req,res,next){
   next();
 });
 
-
 router.get('/empath-analysis', function (req, res) {
-    const API_ENDPOINT = 'https://api.webempath.net/v2/analyzeWav';
-    var formData = {
-    apikey: config.empath_API_key,
-    wav: fs.createReadStream("./resources/0wuqx-scsny.wav") //should be .wav format, shouldn't exceed 5s, 1.9MB  and frequency should be 11025 Hz
-    };
-  
-    request.post({ url: API_ENDPOINT, formData: formData }, function(err, response) {
-    if (err) 
-      console.trace(err);  
-    var respBody = JSON.parse(response.body);
-    console.log("result: " + JSON.stringify(respBody));
-    res.json(JSON.stringify(respBody));
-    });
+  const API_ENDPOINT = 'https://api.webempath.net/v2/analyzeWav';
+  var formData = {
+  apikey: config.empath_API_key,
+  wav: fs.createReadStream("./resources/0wuqx-scsny.wav") //should be .wav format, shouldn't exceed 5s, 1.9MB  and frequency should be 11025 Hz
+  };  
+  request.post({ url: API_ENDPOINT, formData: formData }, function(err, response) {
+  if (err) 
+    console.trace(err);  
+  var respBody = JSON.parse(response.body);
+  console.log("result: " + JSON.stringify(respBody));
+  res.json(JSON.stringify(respBody));
   });
+});
 
 router.post('/add-recording', function(req,res,next){
-    var title = req.body.title ? req.body.title.trim() : '';    
-    if( !req.body || !title) return res.json(apiResponse.errors['miss_req_params'])    
-    if( title.length < 3 ) return res.json(apiResponse.errors['minim_string_length'])
-    firebase.database().ref(`recordings`).push({
-        'title': title  
+  var title = req.body.title ? req.body.title.trim() : '';    
+  if( !req.body || !title) return res.json(apiResponse.errors['miss_req_params'])    
+  if( title.length < 3 ) return res.json(apiResponse.errors['minim_string_length'])
+  firebase.database().ref(`recordings`).push({
+    'title': title  
 	}).catch(function(error) {
-			console.log(error);
-		  res.send("error");
-	});
+    return res.json(apiResponse.customError(error));		
+  });
+  res.json(apiResponse.success('new recording added successfully'));
 });
 
 module.exports = router;
